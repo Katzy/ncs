8ce1a4806b4e8b4784e4701ddcf672745a60acbc
Scott <scottalankatz@gmail.com>
2015-02-02 16:23:03 -0800

    adjusted footer

--- app/models/wrestler.rb
+++ app/models/wrestler.rb
@@ -13,7 +13,7 @@ class Wrestler < ActiveRecord::Base
     CSV.generate(options) do |csv|
       csv << ["weight", "school", "abbreviation", "last name", "first name", "full name", "grade", "wins", "losses", "SEC", "ST", "T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "Comments"]
       all.each do |wrestler|
-        csv << [wrestler.weight, wrestler.school, wrestler.abbreviation, wrestler.last_name, wrestler.first_name, wrestler.first_name + "" + wrestler.last_name, wrestler.grade, wrestler.wins, wrestler.losses, wrestler.section_place, wrestler.state_place, wrestler.tourney1_place, wrestler.tourney2_place, wrestler.tourney3_place, wrestler.tourney4_place, wrestler.tourney5_place, wrestler.tourney6_place, wrestler.tourney7_place, wrestler.tourney8_place, wrestler.comments ]
+        csv << [wrestler.weight, wrestler.school, wrestler.abbreviation, wrestler.last_name, wrestler.first_name, wrestler.first_name + " " + wrestler.last_name, wrestler.grade, wrestler.wins, wrestler.losses, wrestler.section_place, wrestler.state_place, wrestler.tourney1_place, wrestler.tourney2_place, wrestler.tourney3_place, wrestler.tourney4_place, wrestler.tourney5_place, wrestler.tourney6_place, wrestler.tourney7_place, wrestler.tourney8_place, wrestler.comments ]
       end
     end
   end
