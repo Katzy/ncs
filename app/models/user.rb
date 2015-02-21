9208b62faaa63d1c2d664dec3aa64dffd79eef58
Scott <scottalankatz@gmail.com>
2015-02-21 12:50:42 -0500

    changed timout to 24 hours

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :timeout_in => 20.minutes
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :timeout_in => 24.hours
 
   has_many :wrestlers
 
